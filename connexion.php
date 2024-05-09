ffc91852ba63e6cfd54d6a4b52e439437b0eeb1e
Nada20hmd <nada2000elhmadi@gmail.com>
2024-05-09 16:44:26 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -4,24 +4,35 @@
     <meta charset="UTF-8">
     <title>NiceEvents - Connexion</title>
     <link rel="stylesheet" href="style.css">
+    <style> 
+           .bande {
+            background-color: rgba(255, 255, 255, 0.5); 
+            padding: 5px 10px; 
+            border-radius: 5px;
+            font-size: 1.2em; 
+        }
+    </style>
 </head>
 <body>
     <?php include 'header.php'; ?>
 
     <main>
-        <h1>Connexion</h1>
+        <h1><span class="bande">Connexion</span></h1>
 
         <form action="traitement_connexion.php" method="post">
-            <label for="pseudo">Pseudo :</label>
+            <label for="pseudo"><span class="bande">Pseudo :</span></label>
             <input type="text" id="pseudo" name="pseudo" required>
 
-            <label for="motdepasse">Mot de passe :</label>
+            <label for="motdepasse"><span class="bande">Mot de passe :</span></label>
             <input type="password" id="motdepasse" name="motdepasse" required>
 
             <button type="submit">Se connecter</button>
         </form>
 
-        <p>Pas encore inscrit ? <a href="inscription.php">Créer un compte</a></p>
+        <p style="text-align: center;"><span class="bande">Pas encore inscrit ?</span></p>
+        <form action="inscription.php" method="post" style="text-align: center;">
+        <button type="submit" class="subscribe-btn">créer un compte</button>
+    </form>
     </main>
 
     <script src="script.js"></script>
